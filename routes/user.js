9434151d8f66464c7a9336179c2bee129804ac04
William RIBEYRE <williamribeyre@MacBook-Pro-de-William.local>
2017-03-30 12:07:50 +0200

    display static user info

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var axios = require("axios");
+var moment = require("moment");
+
+function formatData(data){
+    var formated = Object.assign({},data);
+
+    formated.created_at = moment(data.created_at).format("YYYY/MM/DD");
+    
+    return formated;
+
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('user', { name: 'wribeyre' });
+ axios.get("https://api.github.com/users/wribeyre")
+     .then(function(response){
+           res.render('user', formatData(response.data));
+     })
+     .catch(function(error){
+         res.render('error',error.response)
+
+     })
+ 
+  
 });
 
 module.exports = router;
